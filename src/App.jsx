eaa06f9d3e98564991cd519369588d5ada3d6951
MikyViz <vizenovsky@gmail.com>
2025-02-26 21:43:07 +0200

    name repair

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
-import Login from './pages/login'
+import Login from './pages/Login'
 import Home from './pages/Home';
 
 function App() {
